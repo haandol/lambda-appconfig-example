b99a5c94da333777de5f0d59b50e570c2e1f8972
Dongkyun Lee <dongkyl@amazon.com>
2021-07-11 00:01:27 +0900

    add timeout

--- infra/lib/functions/app.py
+++ infra/lib/functions/app.py
@@ -15,4 +15,4 @@ def handler(event, context):
 
 def get_configuration(app, env, profile):
     url = f'http://localhost:2772/applications/{app}/environments/{env}/configurations/{profile}'
-    return urllib.request.urlopen(url).read()
+    return urllib.request.urlopen(url, timeout=2).read()
